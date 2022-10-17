6f76e4d3a610f832fd2e4019acf6a64831cfcfc9
obsidianZhu <zhuwanghao@outlook.com>
2022-10-17 22:38:19 +0800

    a

--- pages/api/getPills.js
+++ pages/api/getPills.js
@@ -1,9 +1,9 @@
-import main from "../../lib/main.js";
+import {getPillsApi} from "../../lib/api.js";
 
 export default async function handler(req, res) {
     try {
-        main().then(b =>res.status(200).json({text: 'ok'}));
-
+        const msg = await getPillsApi();
+        res.status(200).json({text: msg});
     } catch (e) {
         res.status(500).json({text: e.toString()});
     }
