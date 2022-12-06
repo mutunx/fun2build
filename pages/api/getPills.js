4db4cce4297e3cb4bdb821dee20f9567e8a4f042
wanghao.zhu <zhuwanghao@outlook.com>
2022-12-06 18:02:33 +0800

    1

--- pages/api/getPills.js
+++ pages/api/getPills.js
@@ -5,6 +5,6 @@ export default async function handler(req, res) {
         const msg = await getPillsApi();
         res.status(200).json({text: msg});
     } catch (e) {
-        res.status(500).json({text: e.toString()});
+        res.status(500).json(e);
     }
 }
